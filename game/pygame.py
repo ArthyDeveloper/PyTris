23a8cedabde05adf6bfc2189d99f171a35101dac
Arthur Queiroz <arthurqoliveiradev@gmail.com>
2025-09-02 17:34:51 -0300

    Adding controls and messing with prototype of dynamic display.

--- game/pygame.py
+++ game/pygame.py
@@ -4,21 +4,42 @@ from settings import (
   game_clock,
   background_color
 )
-import pygame, sys
+import pygame, math, sys
 
 def start():
   pygame.init()
-  pygame.time.Clock().tick(game_clock)
-  screen = pygame.display.set_mode((screen_width, screen_height))
+  screen = pygame.display.set_mode((screen_width, screen_height), pygame.RESIZABLE)
   pygame.display.set_caption("PyTris - A Tetris Clone")
+  circle_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
   while True:
     screen.fill(background_color)
+    circle = pygame.draw.circle(screen, "red", circle_pos, int(screen.get_height() * 0.01))
     
     # Handling Events
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         sys.exit()
+    
+    # Handling Controls
+    keys = pygame.key.get_pressed()
+    # Up and Down
+    if keys[pygame.K_UP]:
+      circle_pos.y -= 5
+    if keys[pygame.K_DOWN]:
+      circle_pos.y += 5
+
+    # Left and Right
+    if keys[pygame.K_LEFT]:
+      circle_pos.x -= 5
+    if keys[pygame.K_RIGHT]:
+      circle_pos.x += 5
+
+    # Debugs
+    #if keys[pygame.K_SPACE]:
+
+    clock = pygame.time.Clock().tick(game_clock) / 1000
+    pygame.display.flip()
 
 if __name__ == "__main__":
   start()
--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 # Pygame Window Configs
-screen_width = 640
+screen_width = 480
 screen_height = 480
-game_clock = 30
+game_clock = 15
 background_color = 0, 0, 0
