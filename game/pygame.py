b2a4feac9170bf6865485362308d2080b6200e26
Arthur Queiroz <arthurqoliveiradev@gmail.com>
2025-09-03 17:30:45 -0300

    Testing grid generation and positioning.

--- game/pygame.py
+++ game/pygame.py
@@ -10,6 +10,20 @@ def start():
   pygame.init()
   pygame.display.set_caption("PyTris - A Tetris Clone")
 
+  # Operations
+  def scale(num):
+    return int(min(game_surface_width, game_surface_height) * num)
+  
+  def generateGrid(cols, rows):
+    out = []
+    for _ in range(rows):
+      row = []
+      for _ in range(cols):
+        row.append([False, "white"])
+      out.append(row)
+    
+    return out
+  
   # Screen and surfaces set up
   screen = pygame.display.set_mode((start_screen_width, start_screen_height), pygame.RESIZABLE)
   screen_width = screen.get_width()
@@ -19,8 +33,8 @@ def start():
   game_surface_width = game_surface.get_width()
   game_surface_height = game_surface.get_height()
 
-  def scale(num):
-    return int(min(game_surface_width, game_surface_height) * num)
+  # Grid - Standard = 10 Cols, 20 Rows
+  block_size = scale(0.02)
 
   while True:
     # Variables
@@ -35,7 +49,18 @@ def start():
     screen.fill(background_color)
     game_surface.fill((50, 50, 50))
     game_surface = pygame.transform.scale(game_surface, (screen_width*0.8, screen_height))
-    pygame.draw.circle(game_surface, 'red', game_surface_center, scale(0.05))
+
+    # Testing Grid
+
+    for i in range(20):
+      x = int(game_surface_width * 0.25)
+      y = int(game_surface_height * 0.3) + (i * block_size)
+
+      pygame.draw.rect(game_surface, 'white', pygame.Rect(x, y, block_size, block_size))
+      pygame.draw.rect(game_surface, 'white', pygame.Rect(x + (11 * block_size), y, block_size, block_size))
+    
+    #pygame.draw.rect(game_surface, 'white', pygame.Rect(game_surface_center[0], game_surface_center[1], 5, 5))
+    #pygame.draw.circle(game_surface, 'red', game_surface_center, scale(0.05))
     screen.blit(game_surface, (math.floor(screen_width*0.1), 0))
     
     # Handling Events
