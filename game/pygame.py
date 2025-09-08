34d72a6386138e014af200f81734896bc54e0cf4
Arthur Queiroz <arthurqoliveiradev@gmail.com>
2025-09-08 17:14:21 -0300

    Creating modules.

--- game/pygame.py
+++ game/pygame.py
@@ -5,29 +5,34 @@ from settings import (
   background_color
 )
 from game.logics import (
+  scale,
   generateGrid
 )
-import pygame, math, sys
+from game.surfaces import (
+  create_game_surface
+)
+from game.inputs import (
+  events,
+  controls
+)
+import pygame, math
 
 def start():
   pygame.init()
   pygame.display.set_caption("PyTris - A Tetris Clone")
 
-  # Operations
-  def scale(num):
-    return int(min(game_surface_width, game_surface_height) * num)
-  
   # Screen and surfaces set up
   screen = pygame.display.set_mode((start_screen_width, start_screen_height), pygame.RESIZABLE)
+
   screen_width = screen.get_width()
   screen_height = screen.get_height()
-  game_surface_pos = [math.floor(screen_width*0.8), screen_height]
-  game_surface = pygame.Surface(game_surface_pos)
+
+  game_surface = create_game_surface(screen_width, screen_height)
   game_surface_width = game_surface.get_width()
   game_surface_height = game_surface.get_height()
 
   # Grid - Standard = 10 Cols, 20 Rows
-  block_size = scale(0.02)
+  block_size = scale(0.02, game_surface_width, game_surface_height)
   cols = 20
   rows = 30
   phantom_rows = 3
@@ -37,8 +42,8 @@ def start():
     # Variables
     screen_width = screen.get_width()
     screen_height = screen.get_height()
-    game_surface_pos = [math.floor(screen_width*0.8), screen_height]
-    game_surface = pygame.Surface(game_surface_pos)
+
+    game_surface = create_game_surface(screen_width, screen_height)
     game_surface_width = game_surface.get_width()
     game_surface_height = game_surface.get_height()
     game_surface_center = (game_surface_width//2, game_surface_height//2)
@@ -66,28 +71,9 @@ def start():
     #pygame.draw.circle(game_surface, 'red', game_surface_center, scale(0.05))
     screen.blit(game_surface, (math.floor(screen_width*0.1), 0))
     
-    # Handling Events
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        pygame.quit()
-        sys.exit()
-    
-    # Handling Controls
-    keys = pygame.key.get_pressed()
-    # Up and Down
-    if keys[pygame.K_UP]:
-      circle_pos.y -= 5
-    if keys[pygame.K_DOWN]:
-      circle_pos.y += 5
-
-    # Left and Right
-    if keys[pygame.K_LEFT]:
-      circle_pos.x -= 5
-    if keys[pygame.K_RIGHT]:
-      circle_pos.x += 5
-
-    # Debugs
-    #if keys[pygame.K_SPACE]:
+    # Handling Events and Controls
+    events(screen_width, screen_height)
+    controls()
 
     clock = pygame.time.Clock().tick(game_clock) / 1000
     pygame.display.flip()
