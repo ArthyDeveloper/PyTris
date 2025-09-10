875e3e381b8053abb2ab5ba0409c8a30ba4bed61
Arthur Queiroz <arthurqoliveiradev@gmail.com>
2025-09-10 17:55:01 -0300

    Obs: Border positioning is currently broken, top priority.
    Code refactored so no more for loop to generate barrier, also, game grid is now centered on screen.
    Also has diferentiation between block_size and border_size and receives values from settings.py file.

--- game/surfaces.py
+++ game/surfaces.py
@@ -1,3 +1,8 @@
+from settings import (
+  grid_columns as cols,
+  grid_rows as rows,
+  grid_phantom_rows as phantom_rows
+)
 import pygame, math
 
 def create_game_surface(screen_width, screen_height):
@@ -10,21 +15,20 @@ def scale_game_surface(surface, screen_width, screen_height):
 def get_dimensions(surface):
   return (surface.get_width(), surface.get_height())
 
-def render_ui(panel, surface, grid, cols, rows, phantom_rows, block_size):
+def render_ui(panel, surface, grid, block_size, border_size):
   match panel:
     case 'default':
       surface_width, surface_height = get_dimensions(surface)
       # Game Grid
       for row_idx, row in enumerate(grid):
         for col_idx, col in enumerate(row):
-          x = int((surface_width * 0.25) + col_idx * block_size + block_size)
-          y = int(surface_height * 0.3 - phantom_rows * block_size) + (row_idx * block_size)
+          x = int(surface_width * 0.5 - cols * block_size // 2 + col_idx * block_size + block_size)
+          y = int(surface_height * 0.5 - rows * block_size // 2 + row_idx * block_size)
           pygame.draw.rect(surface, col[1], pygame.Rect(x, y, block_size, block_size))
 
       # Grid Barrier
-      for i in range(rows):
-        x = int(surface_width * 0.25)
-        y = int(surface_height * 0.3) + (i * block_size)
-
-        pygame.draw.rect(surface, 'white', pygame.Rect(x, y, block_size, block_size))
-        pygame.draw.rect(surface, 'white', pygame.Rect(x + ((cols+1) * block_size), y, block_size, block_size))
+      x = int(surface_width * 0.5 - cols * block_size // 2)
+      y = int(surface_height * 0.5 - rows * block_size // 2 + border_size * phantom_rows)
+      pygame.draw.rect(surface, 'white', pygame.Rect(x, y, border_size, border_size * rows)) # Left Barrier
+      pygame.draw.rect(surface, 'white', pygame.Rect(x + ((cols+1) * border_size), y, border_size, border_size * rows)) # Right Barrier
+      pygame.draw.rect(surface, 'white', pygame.Rect(x, y + border_size * rows, border_size * (cols + 2), border_size)) # Bottom Barrier
