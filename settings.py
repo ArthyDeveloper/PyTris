3b72d8e549a95c42e9d2c07a374f1194ba82fbab
Arthur Queiroz <arthurqoliveiradev@gmail.com>
2025-09-10 17:53:04 -0300

    Adding new settings.

--- settings.py
+++ settings.py
@@ -3,3 +3,10 @@ start_screen_width = 480
 start_screen_height = 480
 game_clock = 15
 background_color = 0, 0, 0
+
+block_size = 2
+border_size = 1
+grid_columns = 10
+grid_rows = 20
+grid_phantom_rows = 3
+grid_color = 'gray10'
