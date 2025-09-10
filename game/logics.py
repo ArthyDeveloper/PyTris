8dbf0f1ced1f31334680e6bf08483bf7b0b584bd
Arthur Queiroz <arthurqoliveiradev@gmail.com>
2025-09-10 17:53:38 -0300

    Now receives data from settings.py file and receives Int as num on scale function.

--- game/logics.py
+++ game/logics.py
@@ -1,3 +1,9 @@
+from settings import(
+  grid_color,
+  grid_columns,
+  grid_rows,
+  grid_phantom_rows
+)
 import os
 # Clear console
 def clear_console():
@@ -5,13 +11,13 @@ def clear_console():
 
 # Resolution Scale
 def scale(num, width, height):
-  return int(min(width, height) * num)
+  return int(min(width, height) * num//100)
 
 # Generate Grid
-def generateGrid(cols: int, rows: int, phantom_rows: int):
+def generateGrid(cols: int = grid_columns, rows: int = grid_rows, phantom_rows: int = grid_phantom_rows):
   grid = []
-  default_grid_block = [False, "white"]
-  phantom_grid_block = [False, "red"]
+  default_grid_block = [False, grid_color]
+  phantom_grid_block = [False, grid_color]
 
   # Generating Phantom Rows
   for _ in range(phantom_rows):
